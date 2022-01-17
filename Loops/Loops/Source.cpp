b633d18ac9bac31485f74961036be0417a0a25bd
Pablo Di Taranto <73130438+PabloDiTaranto@users.noreply.github.com>
2022-01-17 14:36:11 -0300

    da

---
+++ Loops/Loops/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	for (int i = 0; i < 150;  i+= 5)
+	{
+		cout << i << endl;
+	}
+
+
+
+	/*double numberPi = 3.14159;
+	double numberE = 2.718281828;
+
+	int count = 0;
+
+	bool condition = true;
+
+	do
+	{
+		cout << "The number Pi is: " << numberPi << endl;
+		cout << "The number E is: " << numberE << endl;
+		cout << "Count: " << count << endl;
+		cout << "Pi + E * count = " << numberPi + numberE * count << endl;		
+
+		count++;
+
+		if (count <= 100) {
+			condition = true;
+		}
+		else
+		{
+			condition = false;
+		}
+
+	} while (condition);
+	*/
+	system("pause");
+}
